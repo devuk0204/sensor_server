7c08229bda23e548e7410b32106cea4ebded3865
devuk <devuk0204@gmail.com>
2023-06-21 11:38:29 +0900

    req body resolv

--- routes/page.js
+++ routes/page.js
@@ -1,7 +1,7 @@
 const express = require('express');
 // const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
 const { Op } = require('sequelize');
-const { Sensor_plant, Analysis_result, Analysis_month } = require('../models');
+const { Sensor_plant, Analysis_result, Analysis_month, Analysis_minute } = require('../models');
 
 const router = express.Router();
 
@@ -93,9 +93,9 @@ router.get('/environment', async(req, res, next) => {
 
 router.post('/height', async(req, res) => {
   const { height } = req.body;
+  console.log(height)
   try {
     const plants = await Sensor_plant.findOne({});
-    console.log(1)
     await Analysis_month.create({
       register_no: plants.register_no,
       p_name: plants.p_name,
@@ -104,7 +104,6 @@ router.post('/height', async(req, res) => {
       temperature: 1,
       soil_humidity: 1,
     });
-    console.log(2)
     await Sensor_plant.update({
       p_height: height
     }, {
@@ -118,4 +117,34 @@ router.post('/height', async(req, res) => {
     next(error);
   }
 });
+
+router.post('/sensor', async(req, res) => {
+  const { temperature, humidity, soil_humidity } = req.body;
+  console.log('-------------------------------------------');
+  console.log(req.body);
+  try{
+    const plants = await Sensor_plant.findOne({});
+    await Analysis_minute.create({
+      register_no: plants.register_no,
+      p_name: plants.p_name,
+      humidity: humidity,
+      temperature: temperature,
+      soil_humidity: soil_humidity
+    });
+    await Sensor_plant.update({
+      current_humidity: humidity,
+      current_temperature: temperature,
+      current_soil_humidity: soil_humidity
+    }, {
+      where: {
+        register_no: plants.register_no
+      }
+    });
+    res.send({result: 'success'});
+  } catch(error) {
+    console.error(error);
+    next(error);
+  }
+});
+
 module.exports = router;
